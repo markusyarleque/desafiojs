73e985966bfb788a40d8f243d35b6f627f0969e9
MarkusYarleque <markus.yarleque@gmail.com>
2022-12-23 00:01:21 -0500

    Comparar Arryas

---
+++ comparar_arrays.js
@@ -0,0 +1,18 @@
+//Función para evaluar si dos arreglos son exactamente iguales
+function compararArrays(array1,array2){
+    if(array1.length != array2.length){
+        return false;
+    }
+    for(let i = 0; i < array1.length; i++){
+        if(array1[i] !== array2[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(compararArrays([1,2,3], [4,5,6])); //false
+console.log(compararArrays([1,2,3], [4,5,6,7,8,9])); //false
+console.log(compararArrays([1,2,3], [1,2,3])); //true
+console.log(compararArrays([0,1,2], [0,1,2])); //true
+console.log(compararArrays([1,2,3], ['1','2','3'])); //false
--- numero_primo.js
+++ numero_primo.js
@@ -1,18 +1,16 @@
 //Función para evaluar si un número es Primo o no.
 function esPrimo(num){
-    
     if(num <= 1){
         return false;
     }
-
     for(let i = 2; i < num; i++){
         if(num % i == 0){
             return false;
         }
     }
-
     return true;
 }
+
 console.log(esPrimo(2)); //true
 console.log(esPrimo(7)); //true
 console.log(esPrimo(10)); //false
