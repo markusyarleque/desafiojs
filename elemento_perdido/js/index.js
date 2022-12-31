14933a4bd049fb91325d30f0136965226ce26daf
MarkusYarleque <markus.yarleque@gmail.com>
2022-12-31 03:28:38 -0500

    Buscando el elemento perdido

---
+++ elemento_perdido/js/index.js
@@ -0,0 +1,22 @@
+// Buscar elemento dentro del DOM utilizando recursión.
+
+const elemento = 'Elemento 9';
+let encontrado = false;
+
+function buscarElemento(root){
+    if(root.innerHTML === elemento || root.innerText === elemento){
+        encontrado = true;
+        return;
+    }
+
+    if(root.children && root.children.leght === 0){
+        return;
+    }
+
+    for(const hijo of root.children){
+        buscarElemento(hijo);
+    }
+}
+
+buscarElemento(document.body);
+console.log(encontrado);
